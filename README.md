b9ce18ef5e13fceb23e500437bda92f0081c9b5a
petavy@gmx.net <petavy@gmx.net>
2019-04-15 22:00:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## TICKER
+### Project description of TICKER
+This is the automatic generated project description of TICKER. Stay tuned for updates :-)
+### License
+All software is copyright and protected by the Apache License, Version 2.0.
+https://www.apache.org/licenses/LICENSE-2.0
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+
+	"github.com/mpetavy/common"
+)
+
+func start() error {
+	common.Info("ticker start!!")
+	return nil
+}
+
+func stop() error {
+	common.Info("ticker stop!!")
+	return nil
+}
+
+func tick() error {
+	common.Info("ticker tick!!")
+	return nil
+}
+
+func main() {
+	defer common.Cleanup()
+
+	common.New(&common.App{"ticker", "1.0.24", "2018", "service demo", "mpetavy", common.APACHE, "https://github.com/mpetavy/worktime", true, nil,start, stop, tick, time.Duration(5) * time.Second}, nil)
+	common.Run()
+}
