86e19d128d16f8d2a304da8de208ebfb061e83c4
mpetavy <mpetavy>
2023-02-06 19:23:56 +0100

    common@v1.5.26 update

--- main.go
+++ main.go
@@ -7,27 +7,21 @@ import (
 )
 
 func init() {
-	common.Init(true, "1.0.24", "", "", "2018", "service demo", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, start, stop, run, 0)
-
-	common.Events.NewFuncReceiver(common.EventFlagsParsed{}, func(ev common.Event) {
-		if common.IsRunningAsService() {
-			common.App().RunTime = time.Second
-		}
-	})
+	common.Init("1.0.24", "", "", "2018", "service demo", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, start, stop, run, time.Second)
 }
 
 func start() error {
-	common.Info("ticker START!!")
+	common.Info("%s ticker START!!", time.Now().Format(common.DateTimeMilliMask))
 	return nil
 }
 
 func stop() error {
-	common.Info("ticker STOP!!")
+	common.Info("%s ticker STOP!!", time.Now().Format(common.DateTimeMilliMask))
 	return nil
 }
 
 func run() error {
-	common.Info("ticker TICK!!")
+	common.Info("%s ticker tick!!", time.Now().Format(common.DateTimeMilliMask))
 	return nil
 }
 
